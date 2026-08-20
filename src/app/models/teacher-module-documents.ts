82fb864e16f20024c44fd6cc667667986845217d
erica-dev45 <erica.gratia6@gmail.com>
2026-08-20 10:54:43 +0100

    creating teacher mdules and adding the model

---
+++ src/app/models/teacher-module-documents.ts
@@ -0,0 +1,9 @@
+export interface TeacherModuleDocument {
+  id: string;
+  title: string;
+  description: string;
+  typeLabel: string;
+  typeIcon: string;
+  fileSizeLabel: string;
+  dateLabel: string;
+}
