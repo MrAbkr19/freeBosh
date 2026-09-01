62e407d16bf83dc4795a8bbbc8e330c276e58a63
erica-dev45 <erica.gratia6@gmail.com>
2026-09-01 17:10:25 +0100

    changement pour allways data

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -10,6 +10,6 @@
 export const environment = {
   production: true,
   useMockApi: true,
-  apiUrl: 'https://freebosh-production.up.railway.app',
+  apiUrl: 'https://codely.alwaysdata.net',
   mockDataUrl: '/api/db.json',
 };
