eb361a0ff5b30b789378b24dc862877a6338ffe0
erica-dev45 <erica.gratia6@gmail.com>
2026-09-18 23:18:44 +0100

    fix: update auth.js import to point to prisma-client.js

--- backend/prisma-client.js
+++ backend/prisma-client.js
@@ -1,21 +1,6 @@
-// const { PrismaClient } = require('@prisma/client');
-// const { PrismaPg } = require('@prisma/adapter-pg');
-// const { Pool } = require('pg');
-
-// const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-// const adapter = new PrismaPg(pool);
-// const prisma = new PrismaClient({ adapter });
-
-// module.exports = { prisma };
-
 require('dotenv').config();
-
-
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
 
+const prisma = new PrismaClient();
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
-
-module.exports = { prisma };
+module.exports = prisma;
