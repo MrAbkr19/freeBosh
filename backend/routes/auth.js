d6f2793d44dc5ca89f472f75b7b9ed1617ff42be
erica-dev45 <erica.gratia6@gmail.com>
2026-09-18 17:13:17 +0100

    fixed the package json for the scripts of the prisma generator and also noticed that the auth still pointed to my test db file

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { db, initDb } = require('../db');
+// const { db, initDb } = require('../db');
 const { prisma } = require('../prisma-client');
 const { requireAuth } = require('../middleware/auth-middleware');
 
